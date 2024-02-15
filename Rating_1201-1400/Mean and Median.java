d270e495bdd5a84902a61b306ae8dfcb54ef7d39
MD.Maruf Hossain <148317594+maruf-hossain74@users.noreply.github.com>
2024-02-15 15:50:38 +0600

    START55_1371_Rating

--- Rating_1201-1400/Mean and Median.java
+++ Rating_1201-1400/Mean and Median.java
@@ -6,7 +6,7 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
+	// your code goes here
         Scanner input = new Scanner(System.in);
         int t = input.nextInt();
         input.nextLine();
