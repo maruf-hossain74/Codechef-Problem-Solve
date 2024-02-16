d5471ef04bde12cb073bf8cb489c32b18ae13535
MD.Maruf Hossain <148317594+maruf-hossain74@users.noreply.github.com>
2024-02-16 10:59:27 +0600

    START121_326_Rating

---
+++ Rating_0-1000/Leg Space.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int m = input.nextInt();
+        if(n == m) {
+            System.out.println("NO");
+        }
+        else {
+            System.out.println("YES");
+        }
+	}
+}
