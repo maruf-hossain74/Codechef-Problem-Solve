c56cd56aba38a4f4f81ccfba58475100fa269bea
MD.Maruf Hossain <148317594+maruf-hossain74@users.noreply.github.com>
2024-02-29 16:41:45 +0600

    START123_279_Rating

---
+++ Rating_0-1000/Algomaniac Finals.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        if(n == 17) {
+            System.out.println("YAY");
+        }
+        else {
+            System.out.println("NO");
+        }
+	}
+}
