0a7c2cec36daa4d2ffbc94eef8666befe7af62e5
MD.Maruf Hossain <148317594+maruf-hossain74@users.noreply.github.com>
2024-03-07 21:19:02 +0600

    START124_242_Rating

---
+++ Rating_0-1000/Summer Time.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner input = new Scanner(System.in);
+        int x = input.nextInt();
+        if(x > 35) {
+            System.out.println("YES");
+        }
+        else {
+            System.out.println("NO");
+        }
+	}
+}
