19afe68636a11cc17f4143e5cabf3dd818e075b7
MD.Maruf Hossain <148317594+maruf-hossain74@users.noreply.github.com>
2024-03-21 20:20:29 +0600

    START126_245_Rating

---
+++ Rating_0-1000/AC Please.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int n; cin>> n;
+    if(n > 30) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
