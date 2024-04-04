70f7a84015445af1cae4816c39489fb2afc2bf5a
MD.Maruf Hossain <148317594+maruf-hossain74@users.noreply.github.com>
2024-04-04 11:58:44 +0600

    START_313_Rating
    take two integer n & k. If n+k >= 2000, print "YES" else print "NO";

---
+++ Rating_0-1000/Reach 5 Star.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n, k; cin>> n>> k;
+	if(n + k >= 2000) cout << "YES" << endl;
+	else cout << "NO" << endl;
+}
