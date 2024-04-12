5e7d5eef5deb15c25d18c0c95194f1338f819308
MD.Maruf Hossain <148317594+maruf-hossain74@users.noreply.github.com>
2024-04-12 23:15:49 +0600

    START129_329_Rating
    take two input n & k.
    
    it n is greater then k print "FREEKICK"
    
    else print "PENALTY"

---
+++ Rating_0-1000/Football Training.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int n, k; cin>> n>> k;
+    if(n > k) cout << "FREEKICK" << endl;
+    else cout << "PENALTY" << endl;
+}
