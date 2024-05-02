146830713ba978e00560ecb2576020f8f54c13e7
MD.Maruf Hossain <148317594+maruf-hossain74@users.noreply.github.com>
2024-05-02 12:41:15 +0600

    START_132_354_Rating

---
+++ Rating_0-1000/ICE CREAM.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int n, k; cin>> n>> k; 
+    if(n*2 <= k) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
