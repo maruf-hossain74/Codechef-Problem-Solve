8d7729355363fd419d078cdc0fb49bcb9b73a1f9
MD.Maruf Hossain <148317594+maruf-hossain74@users.noreply.github.com>
2024-06-19 23:04:32 -0700

    START25_1598_Rating

---
+++ Rating_1501-1600/Equal MEX.cpp
@@ -0,0 +1,89 @@
+/*$$$$$$$$ বিসমিল্লাহির রাহমানির রাহিম $$$$$$$
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ JATIYA KABI KAZI NAZRUL ISLAM UNIVERSITY
+$$$$$$$$$ MD. MARUF HOSSAIN $$$$$$$$$$$$$$
+$$$$$$$$$ DEPARTMENT OF CSE $$$$$$$$$$$$$$
+$$$$$$$$$ SEASION: 2021-2022 $$$$$$$$$$$*/
+
+#include<bits/stdc++.h>
+#include<algorithm>
+#include<string.h>
+#include<math.h>
+#include<iomanip>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+#define pi      3.1415926535897932384626433832795028841971693993751058
+#define uni(x)    unique((x).begin(),(x).begin()+(x).size())-(x).begin()
+#define ub(x,n)    upper_bound((x).begin(),(x).end(),(n))-(x).begin()
+#define lb(x,n)    lower_bound((x).begin(),(x).end(),(n))-(x).begin()
+#define input(x) scanf("%d",&(x))
+#define int int64_t
+#define endl "\n"
+#define mod 1000000007
+#define chars "0abcdefghijklmnopqrstuvwxyz"
+
+string dtob(int n){ string m = ""; while(n!=0){ if(n%2==0) m='0'+m; else m='1'+m; n/=2; } return m;}
+
+int btod(string s) { int sz = s.size(),des = 0; reverse(s.begin(),s.end());
+    for(int i = 0; i<sz; i++) if(s[i] == '1') des += pow(2,i); return des; }
+
+int gcd(int a,int b){
+    int  x = max({a,b}); int  y = min({a,b}); int rem = x%y; while(rem!=0){
+    x = y; y = rem; rem = x%y;} return y; }
+
+int lcm(int x, int y){  return ((x*y)/gcd(x,y)); }
+
+int nPr(int n, int r){ int ans = 1; for(int i = n - r + 1 ; i <= n ; i++) ans*=i; return ans;}
+
+int nCr(int n, int r) { if (r > n) return 0; if (r == 0 || r == n) return 1;
+    return nCr(n - 1, r - 1) + nCr(n - 1, r); }
+
+bool prime_check(int n) { if(n<2) return false; else if(n <4) return true;
+    else if(n%2==0) return false; else { for(int i = 3; i<=sqrt(n); i+= 2) {
+    if(n%i==0) return false; } return true; }}
+
+bool int_palindrome(int n) { int int1 = n , int2 = 0; while( n!=0 ) {
+    int2 = int2*10 + (n%10); n /= 10; } if(int1 == int2 ) return true;
+     else return false;}
+
+bool array_Palindrome(const vector<int>& vec) { int start = 0; int end = vec.size() - 1;
+    while (start < end) { if (vec[start] != vec[end]) { return false; } start++; end--;} return true;}
+
+int factorial(int n) {int ans = 1; while(n>=1) {ans *= n; n--;} return ans;}
+
+int sum_of_digit(int n) { int sum = 0; while(n != 0) { sum += n % 10; n /= 10; } return sum;}
+
+int binary_search(vector<int> &A, int x) { int mn = 0, mx = A.size() - 1; int val = -1; while(mn <= mx) {
+        int mid = (mn + mx) / 2; if(A[mid] < x) mn = mid +1; else if(A[mid] == x) return mid;
+        else { mx = mid -1; } } return val;}
+int power(int n, int k) { int ans = 1;while(k--) ans *= n; return ans; }
+
+int t = 1;
+int32_t main() {
+	ios::sync_with_stdio(0),cin.tie(0),cout.tie(0),cin >> t;
+    //cin >> t;
+    while(t--) {
+        int n,x; cin>> n;
+        map<int,int> mp;
+        bool ans = true;
+        for(int i = 0; i < 2*n; i++) {
+            cin>> x;
+            mp[x]++;
+        }
+        for(int i = 0; i <= n+1; i++) {
+            if(mp[i] == 0) {
+                break;
+            }
+            else if(mp[i] == 1) {
+                ans = false;
+                break;
+            }
+        }
+        if(ans) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+	return 0;
+}
